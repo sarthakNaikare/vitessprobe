919a508139969dd8f2a8da1b61b0eda81142062a
Sarthak Naikare <sarthaknaikare@gmail.com>
2026-05-06 20:24:14 +0000

    Remove non-existent useEventListener and unused drawRoundRect

--- frontend/src/components/ForceGraph.tsx
+++ frontend/src/components/ForceGraph.tsx
@@ -85,19 +85,6 @@ export default function ForceGraph({ nodes, edges, onNodeClick }: Props) {
 
     const NODE_R = 32
 
-    const drawRoundRect = (ctx: CanvasRenderingContext2D, x: number, y: number, w: number, h: number, r: number) => {
-      ctx.beginPath()
-      ctx.moveTo(x + r, y)
-      ctx.lineTo(x + w - r, y)
-      ctx.quadraticCurveTo(x + w, y, x + w, y + r)
-      ctx.lineTo(x + w, y + h - r)
-      ctx.quadraticCurveTo(x + w, y + h, x + w - r, y + h)
-      ctx.lineTo(x + r, y + h)
-      ctx.quadraticCurveTo(x, y + h, x, y + h - r)
-      ctx.lineTo(x, y + r)
-      ctx.quadraticCurveTo(x, y, x + r, y)
-      ctx.closePath()
-    }
 
     const tick = () => {
       const { nodes: sn, edges: se } = stateRef.current
